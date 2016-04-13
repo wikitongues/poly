50a139b6cea33018ff4cc9fed33baec62b1c613a
Freddie Andrade <faandrade@gmail.com>
2016-04-13 19:56:27 -0400

    started working on styles for profile page

--- app/assets/javascripts/components/Profile.es6.jsx
+++ app/assets/javascripts/components/Profile.es6.jsx
@@ -6,13 +6,59 @@ Profile = React.createClass( {
       languages: ['swiss', 'german', 'french', 'english', 'portuguese'],
       books: [
         {
-          title: 'useful phrases in swiss german',
-          id: 1
+          title: 'Useful phrases in Swiss German',
+          id: 1,
+          source_language:'English',
+          target_language:'Swiss German',
+          phrases:17,
+          favorites:4,
+          shares:4
         },
         {
-          title: 'other cool book',
-          id: 2
-        }
+          title: 'Intro to Swiss German Sign Language',
+          id: 2,
+          source_language:'English',
+          target_language:'Swiss German Sign Language',
+          phrases:22,
+          favorites:19,
+          shares:0
+        },
+        {
+          title: 'Welcome to Switzerland!',
+          id: 3,
+          source_language:'English',
+          target_language:'Swiss German',
+          phrases:43,
+          favorites:2,
+          shares:1
+        },
+        {
+          title: 'Afrikaans workbook',
+          id: 4,
+          source_language:'English',
+          target_language:'Afrikaans',
+          phrases:9,
+          favorites:0,
+          shares:2
+        },
+        {
+          title: 'Useful Afrikaans phrases',
+          id: 5,
+          source_language:'English',
+          target_language:'Afrikaans',
+          phrases:24,
+          favorites:3,
+          shares:0
+        },
+        {
+          title: 'Bem vindos à Suíssa',
+          id: 5,
+          source_language:'Português',
+          target_language:'Suíço-alemão',
+          phrases:30,
+          favorites:0,
+          shares:64
+        },
       ]
     }
   },
@@ -26,7 +72,31 @@ Profile = React.createClass( {
     let books = []
     this.state.books.forEach(function (book) {
       books.push(
-        <a href={'/books/' + book.id}>{book.title}</a>
+        <li className="bookEntry">
+          <a href={'/books/'+book.id}>
+          <section className="info">
+            <span className="banner"><img src=""/></span>
+            <section>
+              <p className="title">{book.title}</p>
+            </section>
+            {/*<p className="author">{this.props.userData.email}</p>*/}
+            <section className="details">
+              <span className="source language" title="Source Language">
+                <p>{book.source_language}</p>
+              </span>
+              <div className="icon cardinality bookEntry" alt=""/>
+              <span className="target language" title="Target Language">
+                <p>{book.target_language}</p>
+              </span>
+            </section>
+            {/*<section className="meta">
+              <p>{book.phrases} phrases</p>
+              <p>{book.favorites} favories</p>
+              <p>{book.shares} shares</p>
+            </section>*/}
+          </section>
+         </a>
+        </li>
       )
     })
 
@@ -38,17 +108,21 @@ Profile = React.createClass( {
     return(
       <div className="container">
         <NavBar currentUser={this.props.currentUser}/>
-        <div className="profile">
-          <h3>{this.props.userData.email}</h3>
-          <img src={`http://www.gravatar.com/avatar/${this.props.hashedEmail}?s=200`} />
-          <div className="languages">
-            {langs}
+        <div id="profile">
+          <div className="info">
+            <h3>{this.props.userData.email}</h3>
+            <img src={`http://www.gravatar.com/avatar/${this.props.hashedEmail}?s=200`} />
+            <div className="languages">
+              {langs}
+            </div>
+            <p>member since {createdMonth} {createdYear}</p>
+            <a>follow</a>
           </div>
-          <div className="books">
-            {books}
+          <div className="dashboard">
+            <ul className="content">
+              {books}
+            </ul>
           </div>
-          <p>member since {createdMonth} {createdYear}</p>
-          <a>follow</a>
         </div>
       </div>
     )}
