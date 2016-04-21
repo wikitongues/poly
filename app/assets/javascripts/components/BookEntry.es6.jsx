7db0a45bf58775d230cfff1015b819ab3c1c2c6e
Freddie Andrade <faandrade@gmail.com>
2016-04-21 15:37:31 -0400

    author usernames now display on book-entries

--- app/assets/javascripts/components/BookEntry.es6.jsx
+++ app/assets/javascripts/components/BookEntry.es6.jsx
@@ -1,5 +1,25 @@
 BookEntry = React.createClass ( {
+  renderAuthor: function() {
+    let users = this.props.users
+    let authorName = ""
+    for (var i = users.length - 1; i >= 0; i--) {
+      if(this.props.book.user_id == users[i].id) {
+        authorName = users[i].username
+      }
+    }
+    return (
+      <span>
+      {authorName}
+      </span>
+    )
+  },
+
   render: function() {
+    let createdDate = new Date(this.props.book.created_at),
+    createdYear = createdDate.getUTCFullYear(),
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+    createdMonth = months[createdDate.getMonth()]
+
     return (
        <li className="bookEntry">
         <a href={"/books/"+this.props.book.id}>
@@ -8,7 +28,8 @@ BookEntry = React.createClass ( {
             <section>
               <p className="title">{this.props.book.title}</p>
             </section>
-            <p className="author">Author ID:{this.props.book.user_id}</p>
+            <p className="author">{createdMonth} {createdYear}</p><br/>
+            <p className="author">{this.renderAuthor()}</p>
             <section className="details">
               <span className="source language" title="Source Language">
                 <p>{this.props.book.source_language}</p>
