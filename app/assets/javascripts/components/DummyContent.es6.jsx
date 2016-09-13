6da1475f8fc048ca8b8c6ebb3f882a013d577e11
Freddie Andrade <faandrade@gmail.com>
2016-09-13 14:06:58 -0400

    refactored random dummy content phrase function

--- app/assets/javascripts/components/DummyContent.es6.jsx
+++ app/assets/javascripts/components/DummyContent.es6.jsx
@@ -1,10 +1,8 @@
 DummyContent = React.createClass( {
-  randomPhraseLength: function() {
-    return Math.floor(Math.random() * (36 - 2)) + 2;
-  },
+  randomString: function() {
+    let charLength = Math.floor(Math.random() * (36 - 2)) + 2;
 
-  randomString: function(length) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+    return Math.round((Math.pow(36, charLength + 1) - Math.random() * Math.pow(36, charLength))).toString(36).slice(1);
   },
 
   render: function() {
@@ -13,20 +11,20 @@ DummyContent = React.createClass( {
         <li className="entry">
           <ul>
             <li className="source">
-              <p>{this.randomString(this.randomPhraseLength())}</p>
+              <p>{this.randomString()}</p>
             </li>
             <li className="target">
-              <p>{this.randomString(this.randomPhraseLength())}</p>
+              <p>{this.randomString()}</p>
             </li>
           </ul>
         </li>
         <li className="entry">
           <ul>
             <li className="source">
-              <p>{this.randomString(this.randomPhraseLength())}</p>
+              <p>{this.randomString()}</p>
             </li>
             <li className="target">
-              <p>{this.randomString(this.randomPhraseLength())}</p>
+              <p>{this.randomString()}</p>
             </li>
           </ul>
         </li>
