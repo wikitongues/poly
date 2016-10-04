bc634119279ac4a263353c00ca7db81f5c911779
Freddie Andrade <faandrade@gmail.com>
2016-10-04 17:41:47 -0400

    add Poly detail

--- app/assets/javascripts/components/NewBook.es6.jsx
+++ app/assets/javascripts/components/NewBook.es6.jsx
@@ -40,7 +40,7 @@ NewBook = React.createClass( {
   render: function() {
     return(
       <div className="container">
-        <NavBar currentUser={this.props.currentUser} logo={this.props.logo} search={this.props.search}/>
+        <NavBar currentUser={this.props.currentUser} logo={this.props.logo} detail={this.props.detail} search={this.props.search}/>
         <span className="backgroundElement"></span>
         <div className="book">
           <form onSubmit={this.onSubmit}>
