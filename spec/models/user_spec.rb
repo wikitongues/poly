bedecb70bcd08a8953b7d02d4a6916c92afda38b
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-11-07 16:04:42 -0700

    add small refactor of @authoredBooks in UsersController * moves ActiveRecord query into a method in User; instead of asking Book to find the user and all books associated to that user, can simply find @user.books and order appropriately * adds model test in User for this method * adds fixtures for users and books for testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  fixtures :users, :books
+
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
   it { should have_many(:books) }
   it { should have_many(:favorite_books) }
   it { should have_many(:favorites) }
+
+  scenario "gives a list of authored books, with oldest books appearing first" do
+    expect(users(:faulkner).authored_books).to eq([books(:absalom), books(:sound_fury), books(:august)])
+  end
 end
