c617e8b0ee4b49a3b64853f81ad05843fe4b1252
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-11-28 19:08:49 -0700

    add book spec to test for relationships and validations on Book * last test not passing due to factoryGirl setup issue

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Book, type: :model do
+  it { should have_many :phrase_pairs }
+  it { should belong_to :user }
+  it { should validate_presence_of :user }
+
+  scenario "cleans language names before validation" do
+    book = build(:book,
+                  source_language: "Hebrew",
+                  target_language: "Greek")
+    book.valid?
+    expect(book.source_language).to eq "hebrew"
+    expect(book.target_language).to eq "greek"
+  end
+
+  scenario "destroys dependent phrase pairs when deleted" do
+    book = create(:book_with_phrase_pairs)
+    expect(book.phrase_pairs.count).to eq 3
+    book.destroy
+    execpt(book.phrase_pairs.count).to eq 0 
+  end
+end
