3349f93a606bbe0fce1bbb738c024b432ca6052b
Frederico Andrade <faandrade@gmail.com>
2016-12-16 15:21:52 -0500

    Revert "Revert "79 improve unit test coverage""

---
+++ spec/factories.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:username) { |n| "username#{n}"}
+    sequence(:email) { |n| "person#{n}@example.com"}
+    password "password"
+  end
+
+  factory :book do
+    sequence(:title) { |n| "Great Title #{n}" }
+    source_language "hebrew"
+    target_language "greek"
+    user
+
+    factory :book_with_phrase_pairs, parent: :book do |book|
+      phrase_pairs { build_list :phrase_pair, 3}
+    end
+  end
+
+  factory :phrase_pair do
+    book
+    source_phrase "fortuna audaces iuvat"
+    target_phrase "fortune favors the bold"
+  end
+end
