934eee1bd552405d8564e0962bb7743be9d05f04
Freddie Andrade <faandrade@gmail.com>
2016-12-16 18:38:19 -0500

    fixes wrong action_mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://www.wikitongues.org" }
+  config.action_mailer.default_url_options = { :host => "http://www.poly.wikitongues.org" }
 end
