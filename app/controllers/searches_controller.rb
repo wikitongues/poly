2078be63ff4fbf286b31603e239e54e3f54efeae
Fredward <faandrade@gmail.com>
2017-02-10 13:36:38 -0500

    removes commented our methods

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,36 +11,16 @@ class SearchesController < ApplicationController
           BookSerializer.new(book)
         end
 
-      # @sourceLanguage = Book.where("source_language like ?", q).sort_by{|book| book.created_at}
-      #   .reverse
-      #   .map do |book|
-      #     BookSerializer.new(book)
-      #   end
-
-      # @targetLanguage = Book.where("target_language like ?", q).sort_by{|book| book.created_at}
-      #   .reverse
-      #   .map do |book|
-      #     BookSerializer.new(book)
-      #   end
-
       @user = User.where("username ilike ?", q).sort_by{|user| user.created_at}
         .reverse
         .map do |user|
           UserSerializer.new(user)
         end
-
-      # Make sure aws links dont display
       @phrase = PhrasePair.where("source_phrase || target_phrase ilike ?", q).sort_by{|phrasePair| phrasePair.created_at}
         .reverse
         .map do |phrase|
           PhraseSerializer.new(phrase)
         end
-
-      # @title = Book.where("title like?", q).sort_by{|book| book.created_at}
-      #   .reverse
-      #   .map do |book|
-      #     BookSerializer.new(book)
-      #   end
 
       render 'search/index'
 
