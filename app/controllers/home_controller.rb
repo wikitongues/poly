1c25caf3d3d7f19086144f07f1800d3bba949b46
Chris Voxland <chrisvoxland@gmail.com>
2017-04-27 20:52:15 -0400

    don't unnecessarily pull all users in home controller and include user when pulling up books

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    @books = Book.all.order("created_at DESC").map do |book|
+    @books = Book.all.includes(:user).order("created_at DESC").map do |book|
       BookSerializer.new(book)
     end
-
-    @users = User.all
 
     if current_user
       redirect_to '/dashboard'
     end
-
   end
 end
