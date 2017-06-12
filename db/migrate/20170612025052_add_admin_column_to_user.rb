5f4db443c23cab6f81895dd809afb3f3a19da480
Fredward <faandrade@gmail.com>
2017-06-11 22:52:11 -0400

    adds admin column to users table

---
+++ db/migrate/20170612025052_add_admin_column_to_user.rb
@@ -0,0 +1,5 @@
+class AddAdminColumnToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210204720) do
+ActiveRecord::Schema.define(version: 20170612025052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
