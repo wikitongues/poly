ba058a6c01de7d74909a1d4b4afd7c94fef6fa2a
Fredward <faandrade@gmail.com>
2017-06-17 17:31:34 -0400

    fixes helper name

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -2,7 +2,7 @@ module EmailHelper
   def email_image_tag(image, **options)
     attachments[image] = {
         :data => File.read(Rails.root.join("app/assets/images/#{image}")),
-        :mime_type => "image/png",
+        :mime_type => "image/#{image.split('.').last}",
         :encoding => "base64"
     }
     image_tag attachments[image].url, **options
