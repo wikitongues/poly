8878563df69c0386e77bdc83620918f894b10c86
Fredward <faandrade@gmail.com>
2017-06-17 23:43:03 -0400

    make images aws references

--- app/mailers/user_notifications.rb
+++ app/mailers/user_notifications.rb
@@ -1,5 +1,4 @@
 class UserNotifications < ApplicationMailer
-  add_template_helper(EmailHelper)
   def welcome_new_user(user)
     @user = user
     mail(to: user.email, subject: "Welcome to Poly!")
