45ce76cb8ab6a6a3900162755c0b0dda5c07a475
Amr Adel <amradelkhalil@gmail.com>
2017-10-21 00:18:50 +0200

    Validating writing book server-side

--- app/assets/javascripts/components/Book.es6.jsx
+++ app/assets/javascripts/components/Book.es6.jsx
@@ -124,11 +124,21 @@ class Book extends React.Component {
         success: function () {
           this.cancelEditingBookState();
         }.bind(this),
-        error() {
-          bootbox.alert({
-            message: 'Something went wrong',
-            closeButton: false,
-          });
+        error(error) {
+          server_errors = JSON.parse(error.responseText);
+          array_of_keys_of_errors = Object.keys(server_errors);
+          for( i = 0 ; i < array_of_keys_of_errors.length; i++){
+            nested_keys = Object.keys(server_errors[array_of_keys_of_errors[i]]);
+            for( j = 0 ; j < nested_keys.length; j++){
+              array_of_errors = server_errors[array_of_keys_of_errors[i]][nested_keys[j]];
+              for( k = 0 ; k < array_of_errors.length; k++){
+                bootbox.alert({
+                  message: nested_keys[j] + ' ' + array_of_errors[k],
+                  closeButton: false,
+                });
+              }
+            }
+          }
         },
       });
     } else {
